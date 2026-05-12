b27d26b9bb350777b59bbf717a72da4f671953e6
Aman <aman081118@gmail.com>
2026-05-12 20:48:12 +0530

    Fix: Admin routes missing fixed

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -58,7 +58,11 @@ const Footer = () => {
           </h3>
           <ul className="space-y-3 text-sm text-gray-300">
             <li className="hover:text-white cursor-pointer">
-              <a href="#" target="_blank" rel="noopener noreferrer">
+              <a
+                href="http://sm-cms.64.227.144.236.sslip.io/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
                 Admin login
               </a>
             </li>
